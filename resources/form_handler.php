712c6b00e3e0476bc46c5dac2ab0dede57394200
AntonioRoccoGit <antonio.rocco97@gmail.com>
2023-05-03 14:18:41 +0200

    added folder for style.css

--- resources/form_handler.php
+++ resources/form_handler.php
@@ -13,46 +13,36 @@ $text_censord = str_replace($censored_word, $censored, $text);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 
-<style>
-    p {
-        margin-top: 1rem;
-        line-height: 1.4rem;
-    }
-
-    .spazio {
-        margin-left: 2rem;
-        background-color: lightcoral;
-        padding: .4rem;
-        border-radius: 10px;
-    }
-</style>
-
 <body>
-    <h2>Testo Originale</h2>
-    <p>
-        <?php
-        echo $text;
-        ?>
-        <span class="spazio">
-            <?php
-            echo "Il paragrafo è lungo: " . strlen($text) . " caratteri";
-            ?>
-        </span>
-    </p>
-    <h2>Teso censurato</h2>
-    <p>
-        <?php
-        echo $text_censord;
+    <div class="container">
 
-        ?>
-        <span class="spazio">
+        <h2>Testo Originale</h2>
+        <p>
             <?php
-            echo "Il nuovo paragrafo è lungo: " . strlen($text_censord) . " caratteri";
+            echo $text;
             ?>
-        </span>
-    </p>
+            <span class="spazio">
+                <?php
+                echo "Il paragrafo è lungo: " . strlen($text) . " caratteri";
+                ?>
+            </span>
+        </p>
+        <h2>Teso censurato</h2>
+        <p>
+            <?php
+            echo $text_censord;
+
+            ?>
+            <span class="spazio">
+                <?php
+                echo "Il nuovo paragrafo è lungo: " . strlen($text_censord) . " caratteri";
+                ?>
+            </span>
+        </p>
+    </div>
 </body>
 
 </html>
